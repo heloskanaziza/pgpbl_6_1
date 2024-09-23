cedfa6a87511004e039255e5e5abfe84525f15e3
heloskanaaziza <heloskanaaziza@gmail.com>
2024-09-23 10:53:45 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -73,26 +73,3 @@ export class HomePage {
     L.control.layers(baseMaps).addTo(this.map);
   }
 }
-
-
-// import { Component } from '@angular/core';
-// import * as L from 'leaflet';
-
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: 'home.page.html',
-//   styleUrls: ['home.page.scss'],
-// })
-// export class HomePage {
-
-//   map!: L.Map;
-
-//   constructor() {}
-//   ionViewDidEnter() {
-//     this.map = L.map('mapId').setView([-7.783430927372292, 110.37622194028354], 15)
-//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> constributors'
-//     }).addTo(this.map);
-//   }
-
-// }
